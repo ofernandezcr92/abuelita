492e415c360b8e84b7adab88b71de65311cfcbdc
oscar.fernandez <oscar.fernandez@hulihealth.com>
2015-02-21 16:17:04 -0600

    Transparence

--- js/mail.js
+++ js/mail.js
@@ -6,6 +6,8 @@ function Composer() {
 	this.initComposer = function () {
 		if(!this.init) {
 			if($('.z0').length > 0) {
+				$('body').prepend('<div class="high-light"></div>');
+				$('.z0').parent().addClass("relevant-area");
     			$('.z0').parent().append('<div class="composer-announcement"><span> Hola Kennt </span></div>')
     			$('.z0').children().addClass("composer-button");
     			this.init = true;
