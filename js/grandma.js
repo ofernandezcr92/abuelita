894e21b4c68bf1bcebabe1b9a4bcd999b2e5852a
Kennt Chan <kennt.chan@hulihealth.com>
2015-02-21 12:11:22 -0600

    Listening to page load

--- js/grandma.js
+++ js/grandma.js
@@ -1,7 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    $("body").append('Test');
-    alert(10);
+// document.addEventListener('DOMContentLoaded', function() {
+    // $("body").append('Test');
+    // alert(10);1
     // chrome.tabs.update({
     //     url: "http://www.gmail.com/"
     // });
+// });
+
+chrome.webNavigation.onCompleted.addListener(function() {
+    $("body").append("<div>Hello Oscar!</div>");
+    console.log(12312321414);
 });
